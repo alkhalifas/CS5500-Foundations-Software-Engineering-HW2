698012fbaa74a049c8f07cef7b204fc8ceb508bb
alkhalifas <salehesam@gmail.com>
2023-10-17 19:27:50 -0400

    updated readme

--- src/components/main/questionList/singleQuestion.jsx
+++ src/components/main/questionList/singleQuestion.jsx
@@ -4,7 +4,7 @@ export default function SingleQuestion({ question, onBack }) {
     return (
         <div className="question-detail">
             <h2>{question.title}</h2>
-            {/*<button onClick={onBack}>Back to Questions</button>*/}
+            <button onClick={onBack}>Back to Questions</button>
         </div>
     );
 }
