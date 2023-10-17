346e884044f1ad80bd7194cd7a18b54070a0c176
alkhalifas <salehesam@gmail.com>
2023-10-16 22:16:16 -0400

    added tags page

--- src/components/main/tagsList.js
+++ src/components/main/tagsList.js
@@ -1,13 +1,26 @@
+import React from 'react';
+import dataModel from '../../models/datamodel';
+import "./tagsList.css"
 
 export default function TagsList() {
+    const tags = dataModel.getAllTags();
+
     return (
-        <>
-            <div>
-                Tags List
-
+        <div>
+            <h2>Total Tags: {tags.length}</h2>
+            <div className="tags-container">
+                {tags.map((tag, index) => (
+                    <div key={tag.tid}>
+                        <div className="tag-box">
+                            <a href={`/`} className="tag-link">
+                                {tag.name}
+                            </a>
+                        </div>
+                        {/* Add a dotted line border after every three tags */}
+                        {index % 3 === 2 && <div className="dotted-line" key={`border-${index}`} />}
+                    </div>
+                ))}
             </div>
-
-        </>
-
+        </div>
     );
 }
